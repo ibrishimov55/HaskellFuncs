906da4326c9b9cce765621a6757c226679e76c69
ibrishimov55 <79409148+ibrishimov55@users.noreply.github.com>
2021-11-24 15:58:28 +0200

    Add files via upload

---
+++ basics.hs
@@ -0,0 +1,33 @@
+add2 :: Int -> Int -> Int 
+add2 x y = x+y
+
+mult3 :: Int -> Int -> Int -> Int
+mult3 x y z = x*y*z
+
+max3 :: Int -> Int  -> Int -> Int
+max3 x y z
+    | x >= y && x>= z   = x
+    | y>=z              = y
+    | otherwise         = z
+
+  
+solve :: Float-> Float->Float->[Float]
+solve a b c 
+     |d < 0     = [] 
+     |otherwise = [(-b+sqrt d) / (2*a),(-b-sqrt d) / (2*a)]
+       where 
+     d = b*b-4*a*c
+	 
+	 
+f :: Float-> Float-> Float
+f x y 
+   |cube_x > cube_y = cube_x  
+   |otherwise       = cube_y
+   where 
+   cube_x = cube x 
+   cube_y = cube y 
+   cube :: Float-> Float 
+    cube z = z*z*z   
+   
+   
+      
